1508058e1c553fb078dc2f604e1ff4f60e13d713
LiveinGround <129398571+LiveInGround@users.noreply.github.com>
2025-10-04 11:43:40 +0200

    Updating to 1.21.8

--- src/main/java/fr/liveinground/admin_craft/mutes/Utils.java
+++ src/main/java/fr/liveinground/admin_craft/mutes/Utils.java
@@ -6,10 +6,10 @@ import net.minecraft.server.MinecraftServer;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.server.players.ServerOpList;
 import net.minecraft.server.players.ServerOpListEntry;
-import net.minecraftforge.server.ServerLifecycleHooks;
 
 import fr.liveinground.admin_craft.AdminCraft;
 import fr.liveinground.admin_craft.PlaceHolderSystem;
+import net.neoforged.neoforge.server.ServerLifecycleHooks;
 
 import java.util.List;
 import java.util.stream.Collectors;
